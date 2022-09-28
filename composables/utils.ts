7c22077b3d3c2cb96589cf03d62384a6baa8c00e
ko0hi <ko0hi.4731@gmail.com>
2022-09-29 03:00:32 +0900

    delete old files

--- composables/utils.ts
+++
@@ -1,8 +0,0 @@
-export function roundBy(x, roundby){
-    return Math.round(x / roundby) * roundby
-}
-
-
-export function randInt(max: number, min: number){
-    return Math.floor(Math.random() * (max - min + 1)) + min
-}
