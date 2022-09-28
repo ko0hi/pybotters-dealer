a4d6dffc7c61d007d31fbdf8cb4a43fd2ae99858
ko0hi <ko0hi.4731@gmail.com>
2022-09-29 03:17:14 +0900

    minor

--- src/server/api/limit.post.ts
+++ src/server/api/limit.post.ts
@@ -2,7 +2,6 @@ import {readBody} from "h3";
 
 export default defineEventHandler(async (event) => {
     const body = await readBody(event)
-    console.log(process.env)
     return await $fetch(
         process.env.FASTAPI_URL + "/limit", {
             method: "POST",
